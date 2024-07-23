9390006fc8910e890602cabfb7b60bf06762c15c
KangJiUng <woong236406@gmail.com>
2024-07-23 18:29:15 +0900

    7/23 수업 내용

--- Backend/final/node-admin-app/routes/index.js
+++ Backend/final/node-admin-app/routes/index.js
@@ -11,15 +11,27 @@ router.get("/", function (req, res, next) {
 // 호출 방식: GET 방식
 // 응답 결과: login.ejs 뷰파일 (로그인 화면 웹페이지 /views/login.ejs)
 router.get("/login", function (req, res) {
-  res.render("login.ejs");
+  res.render("login.ejs", { resultMsg: "" });
 });
 
-// 관리자 웹사이트 로그인 정보 처리 라우팅 메서드
+// 관리자 로그인 정보 처리 라우팅 메서드
 // 호출 주소: http://localhost:5001/login
 // 호출 방식: POST 방식
 // 응답 결과: 메인 페이지 이동(http://localhost:5001/main)
 router.post("/login", function (req, res) {
-  res.redirect("/main");
+  const userid = req.body.userid;
+  const password = req.body.password;
+
+  // id, password 체크 후 결과 확인
+  const result = false;
+
+  if (result) {
+    // 정상 로그인 시
+    res.redirect("/main");
+  } else {
+    // 아이디 또는 암호가 틀리면 다시 로그인 페이지 반환
+    res.render("login.ejs", { resultMsg: "로그인 실패" });
+  }
 });
 
 // 메인페이지 요청/응답 라우팅 메서드
