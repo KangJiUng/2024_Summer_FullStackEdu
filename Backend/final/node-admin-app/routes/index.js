db6103be21d9df1465663028095528af5355d19d
KangJiUng <woong236406@gmail.com>
2024-07-23 18:15:50 +0900

    7/23 미션2

--- Backend/final/node-admin-app/routes/index.js
+++ Backend/final/node-admin-app/routes/index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* 임시 메인 페이지 요청처리 라우팅 메서드 */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-// 로그인 화면 요청/응답 라우팅 메서드
+// 관리자 웹사이트 로그인 화면 요청/응답 라우팅 메서드
 // 호출 주소: http://localhost:5001/login
 // 호출 방식: GET 방식
 // 응답 결과: login.ejs 뷰파일 (로그인 화면 웹페이지 /views/login.ejs)
@@ -14,7 +14,7 @@ router.get("/login", function (req, res) {
   res.render("login.ejs");
 });
 
-// 로그인 정보 처리 라우팅 메서드
+// 관리자 웹사이트 로그인 정보 처리 라우팅 메서드
 // 호출 주소: http://localhost:5001/login
 // 호출 방식: POST 방식
 // 응답 결과: 메인 페이지 이동(http://localhost:5001/main)
