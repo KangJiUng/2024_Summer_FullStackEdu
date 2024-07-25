4b437600e00bfdc50b1245d10e2d600ca6b682c8
KangJiUng <woong236406@gmail.com>
2024-07-25 14:41:36 +0900

    7/25 수업 내용

--- Backend/07-24/node-restapi-app/app.js
+++ Backend/07-24/node-restapi-app/app.js
@@ -10,7 +10,17 @@ var usersRouter = require("./routes/users");
 // 게시글 정보처리 전용 RESTful API 라우터 참조하기
 var articleAPIRouter = require("./routes/api/articleAPI");
 
+// 노트 백엔드 어플리케이션 객체 정의 app
 var app = express();
+
+// 전역 노드 어플리케이션 미들웨어 구현 실습
+//app.use(노드 앱에 추가하고 싶은 기능 정의);
+app.use(function (req, res, next) {
+  // 여기에 모든 사용자의 요청이 있을 때마다 실행되어야하는 기능을 구현합니다.
+  console.log("전역 어플리케이션 미들웨어 함수가 호출되었습니다.", Date.now());
+  // 원래 사용자가 요청했던 또는 응답해야하는 다음 프로세스로 흘러가게합니다.
+  next();
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +31,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// 특정 호출주소에 대한 미들웨어 기능 구현
+// 만약에 사용자 http://localhost:3000/user/eddy 라는 주소를 요청해오면
+// 해당 주소를 분석해 관련 응답을 아래 미들웨어에서 매번 처리한다.
+app.use("/user/:id", function (req, res, next) {
+  const uid = req.params.id;
+
+  if (uid == "eddy") {
+    console.log("현재 에디 사용자에 대한 정보가 요청되었습니다.");
+    res.send("당신은 시스템에 접근할 수 없는 사용자입니다.");
+  }
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- Backend/07-24/node-restapi-app/routes/index.js
+++ Backend/07-24/node-restapi-app/routes/index.js
@@ -1,9 +1,51 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 공통 기능 미들웨어 참조하기
+// middleware.js 모듈에서 제공하는 2개의 미들웨어 함수를 참조합니다.
+const { checkParams, checkQuery } = require("./middleware.js");
+
+// 해당 라우터 파일이 호출되면 무조건 실행되는 미들웨어 함수 정의하기
+router.use(function (req, res, next) {
+  console.log("index.js 라우터 파일이 호출될 때마다 실행되는 기능 구현");
+  next();
+  // res.send("모든 응답 반환하기");
+});
+
+// 특정 주소 호출에 대한 미들웨어 기능 추가
+// http://localhost:3000/sample
+router.use(
+  "/sample",
+  function (req, res, next) {
+    console.log("index.js 라우터 파일 미들웨어2 호출", req.originalUrl);
+    next();
+  },
+  function (req, res, next) {
+    console.log("index.js 라우터 파일 미들웨어3 호출", req.method);
+    res.send(req.method);
+  }
+);
+
+/* 
+- 메인 웹페이지 요청과 응답처리 라우팅메서드
+- 호출 주소: http://localhost:3000/
+- 호출 방식: GET 방식
+- 응답 결과: views/index.ejs 뷰파일 웹페이지 내용을 응답 결과로 제공
+ */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+// 호출 주소 예시: http://localhost:3000/test/1
+// checkParams 미들웨어를 요청 이후 응답 전에 먼저 실행하게 하여 특정 로직을 태웁니다.
+// router.get 메서드 실행 -> checkParams() 미들웨어 실행 -> 응답콜백함수 실행
+router.get("/test/:id", checkParams, async (req, res, next) => {
+  res.render("index.ejs", { title: "테스트" });
+});
+
+// 호출 주소 예시: http://localhost:3000/product?category=computer&stock=100
+router.get("/product", checkQuery, async (req, res, next) => {
+  res.render("index.ejs", { title: "테스트" });
 });
 
 module.exports = router;
