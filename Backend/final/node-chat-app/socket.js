587c7e750fa38a30b64da7a4343cd746577947d6
KangJiUng <woong236406@gmail.com>
2024-07-31 18:02:58 +0900

    7/31 수업 내용

--- Backend/final/node-chat-app/socket.js
+++ Backend/final/node-chat-app/socket.js
@@ -8,7 +8,16 @@ const SocketIO = require("socket.io");
 module.exports = (server) => {
   // 서버 소켓의 입출력(In/Out) 메시지 처리 객체 io 생성
   // input 메시지는 웹브라우저에서 들어오는 메시지, output 메시지는 서버소켓에서 웹브라우저로 전송하는 메시지
-  const io = SocketIO(server, { path: "/socket.io" });
+  // const io = SocketIO(server, { path: "/socket.io" });
+
+  // 서버소켓에 대한 CORS 이슈 해결하기
+  const io = SocketIO(server, {
+    path: "/socket.io",
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
   // 자바스크립트에서 on(이벤트핸들러(처리))
   // io 객체에 connection 이벤트가 발생하면 콜백함수를 실행해라.
@@ -26,6 +35,11 @@ module.exports = (server) => {
       // io.emit() 메서드는 서버소켓(io)에 연결된 모든 사용자에게 메시지를 보낼 때 사용
       // io.emit("클라이언트 이벤트수신기명", 클라이언트로 보낼 데이터);
       io.emit("receiveAll", msg);
+    });
+
+    // 템플릿에서 보내온 메시지 수신처리기
+    socket.on("sendAll", function (nickName, message) {
+      io.emit("broadCastAll", nickName, message);
     });
 
     // 지정한 채팅방 개설 및 입장처리 메시지 이벤트 수신기
@@ -57,6 +71,18 @@ module.exports = (server) => {
 
       // io.to("채널명").emit();은 메시지를 보낸 당사자(나)를 포함한 현재 채널의 모든 사용자에게 메시지 전송
       io.to(msgData.channel).emit("receiveChannel", message);
+    });
+
+    // 접속한 채팅방 명시적 퇴장하기
+    socket.on("exit", async (channel, nickName) => {
+      // 나를 제외한 채팅방 내 모든 사용자에게 퇴장사실알림처리
+      socket.to(channel).emit("exitOk", `${nickName} 님이 퇴장하셨습니다.`);
+
+      // socket.leave(채팅방명); : 해당 채팅방 나가기 처리
+      socket.leave(channel); // 채팅방 퇴장 처리
+
+      // 현재 퇴장하는 사용자한테도 메시지 전송
+      socket.emit("exitOk", `채팅방을 퇴장하셨습니다.`);
     });
   });
 };
