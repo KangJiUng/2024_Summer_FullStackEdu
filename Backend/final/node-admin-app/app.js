6977d16c48bf67541058064c60b7c2142ce3387b
KangJiUng <woong236406@gmail.com>
2024-08-05 09:59:44 +0900

    8/2 수업 내용

--- Backend/final/node-admin-app/app.js
+++ Backend/final/node-admin-app/app.js
@@ -4,31 +4,49 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-// 환경설정 파일 구성하기
+//환경설정파일 구성하기
 require("dotenv").config();
+
+//서버 세션 객체 관리 패키지 참조하기
+var session = require("express-session");
 
 var expressLayouts = require("express-ejs-layouts");
 
-// ORM DB 연결 객체 sequelize 참조
+//ORM DB연결객체 sequelize 참조하기
 var sequelize = require("./models/index.js").sequelize;
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
 var adminRouter = require("./routes/admin");
+// var memberRouter = require('./routes/member');
+// var channelRouter = require('./routes/channel');
 var articleRouter = require("./routes/article");
-var channelRouter = require("./routes/channel");
-var memberRouter = require("./routes/member");
-var messageRouter = require("./routes/message");
+// var messageRouter = require('./routes/message');
 
 var app = express();
 
-// mysql과 자동연결처리 및 모델기반 물리 테이블 생성처리제공
+//mysql과 자동연결처리 및 모델기반 물리 테이블 생성처리제공
 sequelize.sync();
+
+//백엔드 앱에서 세션을 사용할수 있게 설정하기
+app.use(
+  session({
+    resave: false, //매번 세션 강제 저장 옵션  로그인시마다 세션값이 변경이 없어도 강제로 저장할지여부
+    saveUninitialized: true, //빈 세션도 저장할지 여부..
+    secret: "testsecret", //세션 아이디를 만들떄 사용한 암호화 키값
+    cookie: {
+      httpOnly: true, //http지원여부
+      secure: false, //https환경에서만 세션정보를 주고받도록 처리할지여부
+      maxAge: 1000 * 60 * 5, //5분동안 서버세션을 유지하겠다.(1000은 1초)
+    },
+  })
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+//레이아웃 설정
 app.set("layout", "layout");
 app.set("layout extractScripts", true);
 app.set("layout extractStyles", true);
@@ -43,11 +61,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
 app.use("/admin", adminRouter);
+// app.use('/member', memberRouter);
+// app.use('/channel', channelRouter);
 app.use("/article", articleRouter);
-app.use("/channel", channelRouter);
-app.use("/member", memberRouter);
-app.use("/message", messageRouter);
+// app.use('/message', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- Backend/final/node-admin-app/routes/index.js
+++ Backend/final/node-admin-app/routes/index.js
@@ -1,24 +1,19 @@
-// index.js 라우터의 용도는 전체 웹사이트의 공통기능에 대한 라우팅 기능을 제공합니다.
-// 기본 접속 주소는  http://localhost:5001로 접근하도록 app.js에서 설정되어있습니다.
+//index.js 라우터의 용도는 전체 웹사이트의 공통기능에 대한 라우팅 기능을 제공합니다.
+//기본접속주소는 http://localhost:5001 로 접근하게 app.js에서 설정되어 있어요.
 var express = require("express");
 var router = express.Router();
 
-// 관리자 암호를 단방향암호화(해시알고리즘)하기 위해 bcryptjs 패키지 참조하기
+//관리자 암호를 단방향암호화(해시알고리즘) 하기위해 bcryptjs패키지 참조하기
 var bcrypt = require("bcryptjs");
 
-// ORM DB 객체 참조
-var db = require("../models/index");
+//ORM DB객체를 참조합니다.
+var db = require("../models/index.js");
 
-// 관리자 암호를 단방향암호화(해시알고리즘)하기 위해 bcryptjs 패키지 참조하기
-var bcrypt = require("bcryptjs");
-
-/*
-- 관리자 웹사이트 로그인 화면 요청/응답 라우팅 메서드
-- 호출 주소: http://localhost:5001/login
-- 호출 방식: GET 방식
-- 응답 결과: login.ejs 뷰파일 (로그인 화면 웹페이지 /views/login.ejs)
-*/
-router.get("/login", async (req, res) => {
+// 관리자 웹사이트 로그인 웹페이지 요청과 응답처리 라우팅메소드
+// http://localhost:5001/login
+router.get("/login", async (req, res, next) => {
+  //아이디/암호가 일치하지 않은경우 다시 로그인뷰를 전달하고
+  //로그인뷰에 결과메시지 데이터를 전달합니다.
   let resultMsg = {
     code: 400,
     msg: "",
@@ -27,50 +22,72 @@ router.get("/login", async (req, res) => {
   res.render("login.ejs", { layout: false, resultMsg });
 });
 
-// 관리자 로그인 정보 처리 라우팅 메서드
-// 호출 주소: http://localhost:5001/login
-// 호출 방식: POST 방식
-// 응답 결과: 메인 페이지 이동(http://localhost:5001/main)
-router.post("/login", async (req, res) => {
+// 관리자가 입력한 아이디/암호를  추출하여 실제 로그인 프로세스를 처리하는 라우팅메소드
+// http://localhost:5001/login
+router.post("/login", async (req, res, next) => {
+  //아이디/암호가 일치하지 않은경우 다시 로그인뷰를 전달하고
+  //로그인뷰에 결과메시지 데이터를 전달합니다.
   let resultMsg = {
     code: 400,
     msg: "",
   };
 
-  // Step1: 관리자 아이디/암호를 추출한다.
+  //Step1: 관리자 아이디/암호를 추출한다.
   const admin_id = req.body.admin_id;
   const admin_password = req.body.admin_password;
 
-  // Step2: 동일한 관리자 아이디 정보를 조회한다.
-  const admin = await db.Admin.findOne({ where: { admin_id, admin_password } });
+  //Step2: 동일한 관리자 아이디 정보를 조회합니다.
+  const admin = await db.Admin.findOne({
+    where: { admin_id: admin_id },
+  });
 
-  // Step3: DB 저장 암호와 관리자 입력 암호를 체크한다.
-  // 동일한 아이디가 존재하는 경우
+  //Step3: DB저장 암호와 관리자 입력 암호를 체크합니다.
+  //동일한 아이디가 존재한경우
   if (admin) {
-    // DB에 저장된 암호와 관리자가 로그인 화면에서 입력한 암호가 일치하는지 체크
-    // bcrypt.compart("로그인화면에서 전달된 암호", DB에 저장된 암호화된 문자열) 메서드는 암호가 같으면 true 반환, 다르면 false 반환
+    //db저장된 암호와 관리자가 로그인화면에서 입력한 암호가 일치하는지 체크
+    //bcrypt.compare('로그인화면에서 전달된 암호',db에저장된암호화된문자열)메소드는 암호가 같으면 true반환,다르면 false반환
     if (bcrypt.compare(admin_password, admin.admin_password)) {
-      // Step4: 아이디/암호가 일치하면 메인페이지로 이동시키고 그렇지 않으면 처리결과 DATA를 login.ejs에 전달한다.
-      res.redirect("/main");
+      //Step4: 아이디 /암호가 일치하면 메인페이지로 이동시키고
+      //그렇지 않으면 처리결과 data를 login.ejs에 전달합니다.
+
+      //Step5: 서버 세션에 저장할 로그인 사용자의 주요정보 설정하기
+      var sessionLoginData = {
+        admin_member_id: admin.admin_member_id,
+        company_code: admin.company_code,
+        admin_id: admin.admin_id,
+        admin_name: admin.admin_name,
+      };
+
+      //express-session 패키지를 설치하고 app.js에설정하면 req 객체에 session속성이 추가됩니다.
+      //req.session 속성에 loginUser 동적속성을 정의하고 값으로
+      //현재 로그인한 사용자의 주요데이터를 저장합니다.
+      req.session.loginUser = sessionLoginData;
+
+      //현재 사용자의 로그인 여부를 동적속성 isLogined를 정의하고 값으로 true를 설정함.
+      req.session.isLogined = true;
+
+      //서버세션을 최종 저장하고 메인페이지로 이동시킵니다.
+      req.session.save(function () {
+        console.log("세션에 저장완료");
+        res.redirect("/main");
+      });
     } else {
-      // 암호가 일치하지 않는 경우
+      //암호가 일치하지 않은경우
       resultMsg.code = 402;
       resultMsg.msg = "암호가 일치하지 않습니다.";
       res.render("login.ejs", { layout: false, resultMsg });
     }
   } else {
-    // 아이디가 일치하지 않는 경우
+    //동일한 아이디가 없는경우
     resultMsg.code = 401;
-    resultMsg.msg = "아이디가 존재하지 않습니다.";
+    resultMsg.msg = "동일한 아이디가 존재하지 않습니다.";
     res.render("login.ejs", { layout: false, resultMsg });
   }
 });
 
-// 메인페이지 요청/응답 라우팅 메서드
-// 호출 주소: http://localhost:5001/main
-// 호출 방식: GET 방식
-// 응답 결과: main.ejs 뷰파일(메인 웹페이지 /views/main.ejs)
-router.get("/main", async (req, res) => {
+//정상적으로 로그인시 보여줄 메인페이지 요청과 응답처리 라우팅메소드
+// http://localhost:5001/main
+router.get("/main", async (req, res, next) => {
   res.render("main.ejs");
 });
 
