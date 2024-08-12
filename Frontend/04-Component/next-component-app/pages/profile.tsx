ba0c432de7e3bc09f8bd726916b87ee45dc47837
KangJiUng <woong236406@gmail.com>
2024-08-12 16:18:14 +0900

    8/12 수업 내용

--- Frontend/04-Component/next-component-app/pages/profile.tsx
+++ Frontend/04-Component/next-component-app/pages/profile.tsx
@@ -1,5 +1,27 @@
+// 자식 컴포넌트 참조하기
+import Personal from "@/components/personal";
+import Company from "@/components/company";
+
 const Profile = () => {
-  return <div>Profile Page</div>;
+  return;
+  <>
+    {/* 자식 요소에 props로 읽기전용 데이터를 전달합니다. */}
+    <Personal
+      name="강지웅"
+      email="test@test.co.kr"
+      phone="010-1234-1234"
+      age={22}
+    >
+      {/* 자식 컴포넌트의 children 값을 정의합니다. */}
+      <b>사용자 기본 프로필</b>
+    </Personal>
+
+    <hr />
+
+    <Company company="충북대" role="학생개발자" address="청주시 서원구">
+      <span>회사 정보</span>
+    </Company>
+  </>;
 };
 
 export default Profile;
