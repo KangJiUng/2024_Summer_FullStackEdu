91f5f376f0b9968c8b855ce2480d82bcb5895248
KangJiUng <woong236406@gmail.com>
2024-08-14 11:55:59 +0900

    8/14 수업 내용

--- Frontend/06-Hooks-Rendering/next-hook-app/pages/blogs/index.tsx
+++ Frontend/06-Hooks-Rendering/next-hook-app/pages/blogs/index.tsx
@@ -1,30 +1,71 @@
 // 컴포넌트 내에서의 데이터 상태관리를 위한 useState 훅 참조하기
 // 현재 컴포넌트의 생애주기(LifeCycle) 관리를 위한 useEffect 훅 참조하기
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
-// **중요: useEffect 훅을 이용할 때는 반드시 프로젝트의 next.config.mjs 파일내 reactStrictMode를 false로 설정해야합니다.**
+// ** 중요: useEffect 훅을 이용할 때는 반드시 프로젝트의 next.config.mjs 파일내 reactStrictMode를 false로 설정해야합니다. **
 // reactStrictMode 설정은 개발시에만 사용되고 서비스/배포와는 무관한 설정입니다.
+
+// 단일 블로그 데이터 타입 정의
+interface BlogItem {
+  id: number;
+  title: string;
+  view_cnt: number;
+  create_date: string;
+}
+
+// 게시글 원본 데이터 목록(Database)
+const originalData: BlogItem[] = [
+  {
+    id: 1,
+    title: "제목1입니데이",
+    view_cnt: 10,
+    create_date: "2024-08-14",
+  },
+  {
+    id: 2,
+    title: "제목2입니데이바이데이",
+    view_cnt: 20,
+    create_date: "2024-08-14",
+  },
+  {
+    id: 3,
+    title: "제목3입니데이식스",
+    view_cnt: 30,
+    create_date: "2024-08-14",
+  },
+  {
+    id: 4,
+    title: "제목4입니데이터",
+    view_cnt: 40,
+    create_date: "2024-08-14",
+  },
+  {
+    id: 5,
+    title: "제목5입니데이데이",
+    view_cnt: 50,
+    create_date: "2024-08-14",
+  },
+];
 
 const BlogList = () => {
   // 검색어 키워드 상태 데이터값 정의 및 초기화
-  const [searchWord, setSearchWord] = useState("");
+  const [searchWord, setSearchWord] = useState<string>("");
 
   // 검색 결과 블로그 목록 상태 데이터 값 정의 초기화
-  const [blogs, setBlogs] = useState([
-    {
-      id: 1,
-      title: "블로그 제목1",
-      view_cnt: 10,
-      create_date: "2024-08-14",
-    },
-  ]);
+  const [blogs, setBlogs] = useState<BlogItem[]>([]);
 
   // 현재 컴포넌트가 최초로 화면에 렌더링되는 시점(mount)에 실행되는 useEffect 훅 정의
   // useEffect(최초 마운팅될 때 실행할 콜백함수, 생애주기 시점 정의-빈 배열 []의 경우 최초 마운팅되는 시점을 말합니다.)
+  // useEffect(실행할 콜백 함수, []); : 해당 컴포넌트의 최초 마운팅 시점과 언마운팅 시점에 대해 프로그래밍 가능합니다.
   useEffect(() => {
     console.log(
       "최초 블로그 조회 화면이 나타나는 시점(마운팅시점)에 호출됩니다."
     );
+
+    // 해당 컴포넌트가 최초(1회) 마운팅될 때 백엔드 REST API를 호출해서 블로그 목록을 조회해옵니다.
+    // 조회해온 블로그 목록데이터를 setBlogs() setter함수를 통해 상태데이터로 저장합니다.
+    setBlogs(originalData);
 
     // 해당 컴포넌트가 사라지는 시점(unmount)에 실행되는 콜백함수(클린업 함수) 정의
     return () => {
@@ -32,9 +73,39 @@ const BlogList = () => {
     };
   }, []);
 
+  // 화면내 변화가 발생할 때마다 실행되는 useEffect 훅 정의하기
+  useEffect(() => {
+    console.log(
+      "화면내에서 상태 데이터가 변경되어 렌더링이 일어날 때마다 실행됩니다."
+    );
+  });
+
+  // 특정 데이터 상태 데이터의 변경을 감지하여 프로그래밍을 구현하고 싶은 경우도 useEffect훅을 사용합니다.
+  // useEffect(실행할 콜백 함수, [감지할 상태 데이터]);
+  // 감지할 상태데이터값이 변경될 때마다 콜백함수가 실행됩니다.
+  useEffect(() => {
+    console.log("검색어가 변경되어 블로그목록을 갱신합니다.");
+    blogSearch();
+  }, [searchWord]);
+
+  // 검색어 기반 블로그 검색 처리함수 정의
+  // 검색 버튼 클릭시 호출되는 함수
+  const blogSearch = () => {
+    let searchResult: BlogItem[] = [];
+
+    if (searchWord.length > 0) {
+      searchResult = originalData.filter((item) =>
+        item.title.includes(searchWord)
+      );
+      setBlogs(searchResult);
+    } else {
+      setBlogs(originalData);
+    }
+  };
+
   return (
     <div>
-      <h1 className="m-4">
+      <h1 className="m-4 text-2xl">
         <b>블로그 조회하기</b>
       </h1>
 
@@ -49,10 +120,17 @@ const BlogList = () => {
         />
         <button
           type="button"
+          onClick={blogSearch}
           className="ml-2 rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
         >
           검색
         </button>
+        <Link
+          href="/"
+          className="ml-2 rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          메인으로 이동
+        </Link>
       </div>
 
       {/* 블로그 검색 결과목록 표시 영역 */}
