c55e72c276e53f09d7f4759084b92b42d8f9fe2d
KangJiUng <woong236406@gmail.com>
2024-08-19 10:18:43 +0900

    8/16 수업 내용

--- Backend/final/node-chat-app/app.js
+++ Backend/final/node-chat-app/app.js
@@ -4,34 +4,35 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-// 환경설정정보 구성
+//환경설정정보 구성하기
 require("dotenv").config();
 
-// 시퀄라이즈 ORM을 이용해 DB서버와 연결작업 진행
+//시퀄라이즈 ORM 을이용해 DB서버와 연결작업 진행
 var sequelize = require("./models/index.js").sequelize;
 
-// RESTful API 서비스 CORS 이슈해결을 위한 CORS 패키지 참조
+//RESTful API 서비스 CORS 이슈해결을 위한 cors 패키지 참조하기
 const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-// 회원정보 관리 RESTful API 라우터파일 참조하기
+//회원정보/게시글 관리 RESTful API 라우터파일 참조하기
 var memberAPIRouter = require("./routes/memberAPI");
+var articleAPIRouter = require("./routes/articleAPI");
 
 var app = express();
 
 //mysql과 자동연결처리 및 모델기반 물리 테이블 생성처리제공
 sequelize.sync();
 
-// 모든 웹사이트/모바일 프론트에서 REST API에 접근할 수 있게 허락
+//모든 웹사이트/모바일 프론트에서 RESTAPI를 접근할수 있게 허락함
 app.use(cors());
 
 //특정 도메인주소만 허가
 // app.use(
 //   cors({
 //     methods: ["GET", "POST", "DELETE", "OPTIONS"],
-//     origin: ["http://localhost:3000", "https://beginmate.com"],
+//     origin: ["http://localhost:3000", "https://naver.com"],
 //   })
 // );
 
@@ -48,8 +49,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-// memberAPIRouter의 기본 호출주소 체계 정의하기
+//memberAPIRouter의 기본 호출주소 체계 정의하기
 app.use("/api/member", memberAPIRouter);
+app.use("/api/article", articleAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -67,8 +69,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-// 노드 앱의 기본 WAS 서비스 포트
-app.set("port", process.env.PORT || 5001);
-
-// 노드앱이 작동되는 서버 객체 생성
-var server = app.listen(app.get("port"), function () {});
+module.exports = app;
