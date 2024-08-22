21960649b15e5521a3b898b36567ac7462ce8f7f
KangJiUng <woong236406@gmail.com>
2024-08-22 17:43:51 +0900

    8/22 수업 내용

--- LangChainJS/next-langchain-app/pages/api/bot/translatebot.ts
+++ LangChainJS/next-langchain-app/pages/api/bot/translatebot.ts
@@ -1,4 +1,4 @@
-// 호출주소: http://localhost:3000/api/bot/simplebot
+// 호출주소: http://localhost:3000/api/bot/translatebot
 // 라우팅 주소는 /api 폴더 아래의 물리적 폴더명과 파일명으로 라우팅 주소가 설정됨
 
 // NextApiRequest 타입은 웹브라우저에서 서버로 전달되는 각종 정보를 추출하는 HTTPRequest 객체(=req)
@@ -45,67 +45,38 @@ export default async function handler(
     // 클라이언트에서 POST 방식으로 요청해오는 경우를 처리
     if (req.method == "POST") {
       // Step1: 프론트엔드에서 사용자 프롬프트 추출하기
+      const role = req.body.role;
       const prompt = req.body.message;
 
       // Step2: LLM 모델 생성하기
       const llm = new ChatOpenAI({
-        model: "gpt-4o"
+        model: "gpt-4o",
         apiKey: process.env.OPENAI_API_KEY,
       });
 
-      // Case1: OpenAI LLM 모델 기반 질의/응답처리하기 (초심플하게 LLM 연동)
-      // const result = await llm.invoke(prompt);
-
-      // Case2: 메시지 객체를 이용해서 LLM 연동하기 - PromptTemplate 1
-      // SystemMessage 객체는 LLM의 역할이나 질문(힌트)에 관련된 주요 정보를 LLM에게 전달하는 객체
-      // HumanMessage 객체는 사용자가 보낸 질문 메시지를 저장해서 LLM에게 전달가능한 객체
-      // const messages = [
-      //   new SystemMessage("당신은 세계적인 수준의 기술문서 작성자입니다."),
-      //   new HumanMessage(prompt),
-      // ];
-
-      // const result = await llm.invoke(messages);
-
-      // Case3: ChatPromptTemplate를 이용한 프롬프트 전달하기
+      // Case1: ChatPromptTemplate를 이용한 프롬프트 전달하기
       // 프롬프트 템플릿이란? LLM에게 전달할 수 있는 다양한 질문 템플릿을 제공함으로써 보다 효율적인 질문 형식을 만들어 LLM에게 제공하여 좋은 답변을 만들 방식 제공
       // // 의도: 좋은 질문이 좋은 답변을 만든다.
-      // const PromptTemplate = ChatPromptTemplate.fromMessages([
-      //   ["system", "당신은 뛰어난 실력을 가진 셰프입니다."],
-      //   ["user", "{input}"],
-      // ]);
-
-      // // template.pipe(LLM모델) : chain 객체 반환(chain은 처리할 작업의 기본 단위)
-      // // chain(처리할작업)을 여러 개 생성하고 chain을 연결하여 로직을 구현하는 방식이 'LangChain'이다!
-      // const chain = PromptTemplate.pipe(llm);
-      // const result = await chain.invoke({ input: prompt });
-
-      // Case4: LLM 응답 결과 메시지는 기본 AIMessage 객체를 반환하지만
-      // 해당 타입을 맞춤형 데이터 타입으로 변환해주는 OutputParser를 이용하여 원하는 포맷으로 변경 가능하다.
-      const OutputParser = new StringOutputParser();
       const PromptTemplate = ChatPromptTemplate.fromMessages([
-        ["system", "당신은 근대사 역사학자입니다."],
+        ["system", role],
         ["user", "{input}"],
       ]);
 
-      // template.pipe().pipe(): 두 개의 chain을 만들고 순차적으로 두 개를 체인 목록을 가진 결과체인을 반환
-      // LLM 모델에 의해 결과메시지(AIMessage)를 받아 StringOutputParser를 통해 문자열로 반환한 결과 제공
-      const chains = PromptTemplate.pipe(llm).pipe(OutputParser);
+      // template.pipe(LLM모델) : chain 객체 반환(chain은 처리할 작업의 기본 단위)
+      // chain(처리할작업)을 여러 개 생성하고 chain을 연결하여 로직을 구현하는 방식이 'LangChain'이다!
+      const chain = PromptTemplate.pipe(llm);
+      const result = await chain.invoke({ input: prompt });
 
-      // outputParser로 인해 result값은 실제 LLM의 응답메시지 문자열이 반환된다.(AIMessage.content)
-      const resultMessage = await chains.invoke({ input: prompt });
+      // Case2: (미션)Systemp, Human Message를 이용한 LLM 호출을 구현해주세요.
 
-      const resultMsg: IMessage = {
-        user_type: UserType.BOT,
-        message: resultMessage,
-        send_date: Date.now().toString(),
-      };
+      // Case3: (미션)StringOutputParser를 이용한 결과물 파싱처리 코드를 작성해주세요.
 
       // 메시지 처리결과 데이터
-      // const resultMsg: IMessage = : result가 AIMessage의 타입인 경우(case1~3에 해당하는 경우만)
-      //   user_type: UserType.BOT,
-      //   message: result.content as string,
-      //   send_date: Date.now().toString(),
-      // };
+      const resultMsg: IMessage = {
+        user_type: UserType.BOT,
+        message: result.content as string,
+        send_date: Date.now().toString(),
+      };
 
       // Step3: API 호출 결과 설정
       apiResult.code = 200;
---
+++ LangChainJS/next-langchain-app/pages/translation.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { IMessage, UserType, ISendMessage } from "@/interfaces/message";
+
+const Translation = () => {
+  // 사용자입력 채팅 메시지 상태값 정의 및 초기화
+  const [message, setMessage] = useState<ISendMessage>({
+    role: "",
+    message: "",
+  });
+
+  // 챗봇과의 채팅이력 상태값 목록 정의 및 초기화
+  const [messageList, setMessageList] = useState<IMessage[]>([]);
+
+  // 메시지 전송 버튼 클릭시 메시지 백엔드 API 전송하기
+  const messageSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const userMessage: IMessage = {
+      user_type: UserType.USER,
+      message: message.message,
+      send_date: Date.now().toString(),
+    };
+
+    // 백엔드로 사용자 입력메시지를 전송하기 전에 사용자 메시지를 메시지목록에 추가하여 화면에 사용자 입력 정보를 출력한다.
+    // 왜? 여기서? 현재 WebSocket 기반 실시간 통신이 아니어서 백엔드는 두 번의 응답을 받을 수 없기 때문
+    setMessageList((prev) => [...prev, userMessage]);
+
+    const response = await fetch("/api/bot/tranlatebot", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(message),
+    });
+
+    if (response.status === 200) {
+      const result = await response.json();
+      setMessageList((prev) => [...prev, result.data]);
+      setMessage({ role: "", message: "" });
+    }
+  };
+
+  return (
+    <div className="m-4">
+      SimpleBot
+      {/* 메시지 입력 전송 영역 */}
+      <form className="mt-4" onSubmit={messageSubmit}>
+        <div>
+          <input
+            type="text"
+            name="role"
+            value={message.role}
+            placeholder="챗봇의 역할(목적)을 지정해주세요."
+            onChange={(e) => {
+              setMessage({ ...message, role: e.target.value });
+            }}
+            className="block rounded-md w-[300px] border-0 py-1 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: text-gray-400"
+          />
+        </div>
+        <div className="flex mt-4">
+          <input
+            type="text"
+            name="message"
+            value={message.message}
+            placeholder="질문을 입력해주세요."
+            onChange={(e) => {
+              setMessage({ ...message, message: e.target.value });
+            }}
+            className="block rounded-md w-[500px] border-0 py-1 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: text-gray-400"
+          />
+          <button
+            type="submit"
+            value={message.message}
+            className="rounded-md bg-indigo-600 px-3 py-2 ml-4 text-sm font-semibold text-white shadow-sm hover: bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            전송
+          </button>
+        </div>
+      </form>
+      {/* 메시지 출력 표시 영역 */}
+      <div>
+        <ul>
+          {messageList.map((msg, index) => (
+            <li key={index}>
+              {msg.user_type}: {msg.message}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Translation;
