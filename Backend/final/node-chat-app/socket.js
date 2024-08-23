bd896e8b8027d19ee5ab64405ee9f27755caedeb
KangJiUng <woong236406@gmail.com>
2024-08-23 18:46:52 +0900

    8/23 수업 내용

--- Backend/final/node-chat-app/socket.js
+++ Backend/final/node-chat-app/socket.js
@@ -1,33 +1,37 @@
-// 메시지 서버 소켓파일입니다.
-// 해당 socket.js 모듈이 메시징 서버 역할을 제공합니다.
+//메시지 서버 소켓파일입니다.
+//해당 socket.js 모듈이 메시징 서버 역할을 제공합니다.
 
-// socket.io 패키지 참조
+//socket.io 팩키지 참조
 const SocketIO = require("socket.io");
 
-// db객체 참조하기
+//db객체 참조하기
 var db = require("./models/index");
 
-// 동적 SQL쿼리를 직접 작성해서 전달하기 위한 참조
+//동적 SQL쿼리를 직접 작성해서 전달하기 위한 참조
 var sequelize = db.sequelize;
 const { QueryTypes } = sequelize;
 
-// OpenAI API 호출을 위한 axiosa 패키지 참조하기
+//OpenAI API 호출을 위한 axios 패키지 참조하기
 const axios = require("axios");
 
-// 파일처리를 위한 filesystem 내장객체 참조하기
+//파일처리를 위한 filesystem 내장객체 참조하기
 const fs = require("fs");
 
-// OpenAI 객체 생성하기
+//OpenAI 객체 생성하기
 const { OpenAI } = require("openai");
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({
+  apikey: process.env.OPENAI_API_KEY,
+});
 
-// socket.js 모듈 기능정의
+//socket.js모듈 기능정의
 module.exports = (server) => {
-  // 서버 소켓의 입출력(In/Out) 메시지 처리 객체 io 생성
-  // input 메시지는 웹브라우저에서 들어오는 메시지, output 메시지는 서버소켓에서 웹브라우저로 전송하는 메시지
-  // const io = SocketIO(server, { path: "/socket.io" });
+  //서버 소켓의 입출력(In/Out) 메시지 처리 객체 io 생성
+  //input 메시지는 웹브라우저에서 들어오는 메시지
+  //output 메시지는 서버소켓에서 웹브라우저로 전송하는 메시지
+  // const io = SocketIO(server,{path:"/socket.io"});
 
-  // 서버소켓에 대한 CORS 이슈 해결하기
+  //서버소켓에 대한 CORS 이슈 해결하기
+  //서버소켓객체 io: 서버소켓과연결된 모든 사용자제어가능
   const io = SocketIO(server, {
     path: "/socket.io",
     cors: {
@@ -36,88 +40,106 @@ module.exports = (server) => {
     },
   });
 
-  // 자바스크립트에서 on(이벤트핸들러(처리))
-  // io 객체에 connection 이벤트가 발생하면 콜백함수를 실행해라.
-  // connection 이벤트는 웹브라우저와 서버소켓과 연결이 완료되면 발생합니다.
-  // socket 객체는 개별 사용자/그룹사용자 단위의 메시지 처리를 담당하는 객체입니다.
+  //자바스크립트에서 on(이벤트핸들러(처리기))
+  //io객체에 connection 이벤트가 발생하면 콜백함수를 실행해라.
+  //connection 이벤트는 웹브라우저와 서버소켓과 연결이 완료되면 발생합니다.
+  //socket객체는 개별 사용자/그룹사용자 단위의 메시지 처리를 담당하는 객체..
   io.on("connection", (socket) => {
-    // socket: 현재 연결된 사용자(웹브라우저)와 서버소켓간 연결 객체
-    // 웹브라우저에서 서버소켓에 broadcast 이벤트 수신기를 호출하면 관련 콜백함수가 실행됩니다.
-    // socket.on("서버소켓 이벤트 수신기명", 처리할콜백함수);
+    //socket은 현재 연결된 사용자(웹브라우저) 서버소켓간 연결 객체
+    //웹브라우저에서 서버소켓에 broadcast라는 이벤트 수신기를 호출하면 관련 콜백함수가 실행된다.
+    //socket.on("서버소켓 이벤트 수신기명",처리할콜백함수):
 
-    // 웹브라우저(클라이언트/프론트엔드)에서 서버소켓에 broadcast 이벤트를 호출하면
-    // 서버소켓이 클라이언트에서 보내준 메시지를 수신하고 콜백함수를 통해 클라이언트로 메시지를 전송(io.emit())합니다.
+    //웹브라우저(클라이언트/프론트엔드)에서 서버소켓에 broadcast라는 이벤트를 호출하면
+    //서버소켓에서 클라이언트에서 보내준 메시지를 수신하고 콜백함수를 통해
+    //서버에서 클라이언트로 메시지를 전송(io.emit())한다.
     socket.on("broadcast", function (msg) {
-      // 현재 메시지 서버에 연결된 모든 사용자들(웹브라우저/프론트엔드웹페이지)에게
-      // 메시지를 전송하는데, 클라이언트 메시지 수신 이벤트명이 receiveAll로 msg데이터를 전송한다.
-      // io.emit() 메서드는 서버소켓(io)에 연결된 모든 사용자에게 메시지를 보낼 때 사용
-      // io.emit("클라이언트 이벤트수신기명", 클라이언트로 보낼 데이터);
+      //현재 메시지 서버에 연결된 모든 사용자들(웹브라우저/프론트엔드웹페이지)에게
+      //메시지를 전송하는데 클라이언트 메시지 수신 이벤트 receiveAll로 msg데이터를 전송한다.
+      //io.emit()메소드는 서버소켓(io) 연결된 모든 사용자에게 메시지를 보낼때 사용..
+      //io.emit('클라이언트 이벤트수신기명',클라이언트로 보낼데이터);
       io.emit("receiveAll", msg);
     });
 
-    // 템플릿에서 보내온 메시지 수신처리기
+    //템플릿에서 보내온 메시지 수신처리기
     socket.on("sendAll", function (nickName, message) {
       io.emit("broadCastAll", nickName, message);
     });
 
-    // 지정한 채팅방 개설 및 입장처리 메시지 이벤트 수신기
-    socket.on("entry", async (channel, nickName) => {
-      // socket.join("채팅방이름");
-      // socket.join("채팅방이름");은 서버 환경에 해당 채팅방 이름으로 채널을 개설하고 현재 입장하는 사용자를 해당 채팅방 사용자로 등록처리한다.
-      // 이미 해당 채널이 개설되어 있으면 신규 개설하지 않고 기존 채널로 입장한다.
+    //지정한 채팅방 개설 및 입장처리 메시지 이벤트 수신기
+    //socket.on('서버측이벤트 수신기명',콜백함수(){});
+    socket.on("entry", async (channel, member) => {
+      //socket.join('채팅방이름');
+      //socket.join('채팅방이름')은 서버환경에 해당 채팅방이름으로
+      //채널을 개설하고 현재 입장하는 사용자를 해당 채팅방 사용자로 등록처리한다.
+      //이미 해당 채널이 개설되어 있으면 신규개설하지 않고 기존 채널로 입장한다.
       socket.join(channel);
 
-      // 현재 접속자를 제외한 해당 채널에 이미 접속한 모든 사용자에게 메시지를 발송한다.
-      // socket.to("채널명").emit();
-      socket
-        .to(channel)
-        .emit("entryOk", `${nickName} 님이 ${channel} 채널에 입장하셨습니다.`);
+      //현재 접속자를 제외한 해당 채널에 이미 접속한 모든사용자에게 메시지를 발송한다.
+      //socket.to('채널명').emit();
 
-      // 현재 채널에 입장하고 있는 사용자에게만 메시지를 발송한다.
-      // socket.emit(); 은 현재 서버소켓을 호출한(입장하는) 사용자에게만 메시지 발송하기
-      socket.emit(
-        "entryOk",
-        `${nickName} 대화명으로  ${channel} 채널에 입장하였습니다.`
-      );
+      socket.to(channel).emit("entryOk", {
+        member_id: member.member_id,
+        name: member.name,
+        profile: member.profile,
+        message: `${member.name}님이 ${channel} 채널에 입장했습니다.`,
+        send_date: Date.now(),
+      });
+
+      //현재 채널에 입장하고 있는 사용자에게만 메시지 발송하기
+      //socket.emit(); 현재 서버소켓을 호출한(입장하는) 사용자에게만 메시지 발송하기
+      socket.emit("entryOk", {
+        member_id: member.member_id,
+        name: member.name,
+        profile: member.profile,
+        message: ` ${member.name} 라는 대화명으로 ${channel} 채널에 입장했습니다.`,
+        send_date: Date.now(),
+      });
     });
 
-    // 채팅방 기준으로 해당 채팅방의 나를 포함한 모든 사용자들에게 메시지 전송
-    // 클라이언트에서 메시지 데이터를 JSON 포맷으로 전송
-    socket.on("channelMsg", async (msgData) => {
-      // 클라이언트로 보낼 메시지 포맷 정의하기
-      const message = `${msgData.nickName}: ${msgData.message}`;
+    //채팅방 기준으로 해당 채팅방의 나를 포함한 모든 사용자들에게 메시지 발송하기
+    //클라이언트에서 메시지 데이터를 json포맷으로 전송한다.
+    socket.on("channelMsg", async (channel, msgData) => {
+      //클라이언트로 보낼 메시지 포맷 정의하기
+      const message = {
+        member_id: msgData.member_id,
+        name: msgData.name,
+        profile: msgData.profile,
+        message: msgData.message,
+        send_date: Date.now(),
+      };
 
-      // io.to("채널명").emit();은 메시지를 보낸 당사자(나)를 포함한 현재 채널의 모든 사용자에게 메시지 전송
-      io.to(msgData.channel).emit("receiveChannel", message);
+      //io.to('채널명').emit()는 현재채널에
+      //메시지를 보낸 당사자(나를) 포함한 현재 채널 모든 접속자(사용자)에게 메시지 보내기
+      io.to(channel).emit("receiveChannel", message);
     });
 
-    // 접속한 채팅방 명시적 퇴장하기
+    //접속한 채팅방 명시적 퇴장하기
     socket.on("exit", async (channel, nickName) => {
-      // 나를 제외한 채팅방 내 모든 사용자에게 퇴장사실알림처리
-      socket.to(channel).emit("exitOk", `${nickName} 님이 퇴장하셨습니다.`);
+      //나를 제외한 채팅방내 모든 사용자에게 퇴장사실알림처리
+      socket.to(channel).emit("exitOk", `${nickName}님이 퇴장했습니다.`);
 
-      // socket.leave(채팅방명); : 해당 채팅방 나가기 처리
-      socket.leave(channel); // 채팅방 퇴장 처리
+      //socket.leave(채팅방명);//해당 채팅방 나가기 처리
+      socket.leave(channel); //채팅방 퇴장처리
 
-      // 현재 퇴장하는 사용자한테도 메시지 전송
-      socket.emit("exitOk", `채팅방을 퇴장하셨습니다.`);
+      //현재 퇴장하는 사용자한테도 메시지 전송하기
+      socket.emit("exitOk", `채팅방을 퇴장했습니다.`);
     });
 
-    // ChatGPT-4o와의 질문하고 답하기처리 실시간 이벤트 수신기 정의
-    // 프론트엔드 소켓에서 호출하는 gpt 서버 이벤트 수신기 정의
+    //ChatGPT-4o와의 질문하고 답하기 처리 실시간 이벤트 수신기 정의
+    //프론트엔드 소켓에서 호출하는 gpt서버 이벤트 수신기 정의
     socket.on("gpt", async (msg) => {
-      // Step0: 사용자가 보내준 메시지데이터를 다시 현재 사용자에게 발송해 화면에 표시하기
-      // socket.emit("gptMessage", msg); : 현재 메시지를 보내온 사용자에게만 서버에서 메시지 발송하기
+      //Step0: 사용자가 보내준 메시지 데이터를 다시 현재사용자에게 발송해 화면에 표시하기
+      //socket.emit('gptMessage',msg); 현재 메시시를 보내온 사용자에게만 서버에서 메시지 발송하기
       socket.emit("gptMessage", msg);
 
-      // Step1: 프론트엔드 소켓에서 전달해준 메시지 데이터 추출하기-prompt
+      //Step1: 프론트엔드 소켓에서 전달해준 메시지 데이터 추출하기-prompt
       const prompt = msg.message;
 
-      // Step2: OpenAI ChatGPT REST API 호출하기
+      //Step2: OpenAI ChatGPT REST API 호출하기
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
         {
-          model: "gpt-4o", // 지원 LLM 모델명: gpt-3.5-turbo, gpt-4, gpt-4o, gpt-4o-mini
+          model: "gpt-4o", //지원LLM모델명:gpt-3.5-turbo,gpt-4,gpt-4o,gpt-4o-mini
           messages: [{ role: "user", content: prompt }],
         },
         {
@@ -128,10 +150,9 @@ module.exports = (server) => {
         }
       );
 
-      // Step3: ChatGPT 응답메시지 반환받기
+      //Step3: ChatGpt 응답메시지 반환받기
       const gptMessage = response.data.choices[0].message.content;
-
-      // Step4: 프론트엔드 소켓으로 GPT응답메시지 데이터 전송하기
+      //Step4) 프론트엔드 소켓으로 GPT응답메시지 데이터 전송하기
       msg.message = gptMessage;
       msg.member_id = 0;
       socket.emit("gptMessage", msg);
--- Frontend/Final/next-platform-app/pages/chat/index.tsx
+++ Frontend/Final/next-platform-app/pages/chat/index.tsx
@@ -1,131 +1,126 @@
-// 사용자간 기초 채팅기능구현 컴포넌트
+//사용자간 기초 채팅기능구현 컴포넌트
 import { useState, useEffect, useContext } from "react";
-import { useRouter } from "next/router";
-import { IMessage } from "@/interfaces/message";
 
-// 채팅 클라이언트 socket 객체 참조하기
-import { socket } from "@/library/socket";
-
-// 전역 컨텍스트 참조하기
+//전역컨텍스트 참조하기
 import { GlobalContext } from "@/library/globalContext";
 
+import { useRouter } from "next/router";
+
+import { IMessage } from "@/interfaces/message";
+
+//채팅 클라이언트 socket객체 참조하기
+import { socket } from "@/library/socket";
+
+//채팅 컴포넌트 정의
 const Chat = () => {
-  // 라우터 객체 생성
+  //라우터 객체 생성
   const router = useRouter();
 
-  // 전역 상태값에서 로그인한 사용자의 정보 조회를 위한 컨텍스트 객체 생성
+  //전역 상태값에서 로그인한 사용자의 정보 조회하기 위해 컨텍스트 객체 생성
   const { globalData, setGlobalData } = useContext(GlobalContext);
 
-  // 사용자 인증토큰정보 관리 상태값 정의하기
+  //사용자 인증토큰정보 관리 상태값 정의하기
   const [memberToken, setMemberToken] = useState<string>("");
 
-  // 현재 접속 채널 상태값 정의하기
+  //현재 접속채널 상태값 정의하기
   const [channel, setChannel] = useState<number>(0);
 
-  // 채팅 메시지 입력 요소 바인딩 텍스트 상태값 정의
+  //채팅 메시지 입력 요소 바인딩 텍스트 상태값 정의
   const [message, setMessage] = useState<string>("");
 
-  // 채팅 메시지 목록(채팅이력정보) 상태값 정의
-  const [messageList, setMessageList] = useState<IMessage[]>([
-    {
-      member_id: 1,
-      name: "둘리",
-      profile: "http://localhost:5000/img/user1.png",
-      message: "하이",
-      send_date: "2024-08-21 10:00:00",
-    },
-    {
-      member_id: 2,
-      name: "지지",
-      profile: "http://localhost:5000/img/user2.png",
-      message: "ㅇㅇ",
-      send_date: "2024-08-21 10:01:00",
-    },
-    {
-      member_id: 3,
-      name: "홍길똥",
-      profile: "http://localhost:5000/img/user3.png",
-      message: "ㅎㅇ~~",
-      send_date: "2024-08-21 10:05:00",
-    },
-  ]);
+  //채팅 메시지 목록(채팅이력정보) 상태값 정의하기
+  const [messageList, setMessageList] = useState<IMessage[]>([]);
 
-  // useEffect 훅은 CSR환경에서 작동되고 useRouter 훅은 SSR/SCR 순서로 2번 작동된다.
-  // useEffect 훅에서 useRouter 훅을 이용하여 URL 키값이 추출안되는 문제는 useRouter.isReady 값을 이용하여 해결 가능
-  // useRouter.isReady 값의 기본은 false -> true로 변경되는 시점에 관련 기능 구현하면 된다.
+  //useEffect훅은 CSR환경에서 작동되고 userRouter훅은 SSR/CSR순서로 2번작동됨.
+  //useEffect훅에서 userRouter훅이용해 URL키값이 추출안되는 문제는  useRouter.isReady값을 이용해 해결가능
+  //useRouter.isReady 값이 기본은 false->true로 변경되는 시점에 관련 기능 구현하면됨..
   useEffect(() => {
-    console.log("현재 URL 주소에서 채널번호 추출하기:", router.query.cid);
-
-    // URL 주소를 통해 사용자 고유번호가 전달된 경우에만 실행
+    console.log("현재 URL주소에서 채널번호 추출하기:", router.query.cid);
+    //URL주소를통해 사용자 고유번호가 전달된 경우에만 실행
     if (router.query.cid != undefined) {
-      // 현재 채널 고유번호 상태값 설정
+      //현재 채널 고유번호 상태값 설정해주기
       setChannel(Number(router.query.cid));
     }
   }, [router.isReady]);
 
-  // 현재 접속 채널 정보가 변경될 때마다 실행되는 useEffect 함수
-  // 채널번호가 바뀌면 바뀐 번호 채널의 채팅방에 입장 처리
+  //현재 접속 채널정보가 변경될때마다 실행되는 useEffect함수
+  //채널번호가 바뀌면 바뀐번호 채널로 채팅방에 입장하기 처리
   useEffect(() => {
-    // 채팅방 입장처리
+    //채팅방 입장처리하기
     console.log("채팅방 채널이 변경되었습니다.", channel);
-
     if (channel > 0) {
+      console.log("채팅방에 입장합니다.", channel);
+      //해당 변경된 채팅방에 입장처리하기
+      socket.emit("entry", channel.toString(), globalData.member);
     }
   }, [channel]);
 
-  // 최초 1회 화면이 렌더링(마운팅)되는 시점에 실행되는 useEffect 함수
-  // 프로젝트 루트에 next.config.mjs 파일내 reactStrictMode 값을 false로 변경해야 정확히 1회만 실행
-  // 채팅 서버와 연결되는 클라이언트 채팅 소켓 객체 생성 및 각종 채팅 이벤트 기능 구현 영역
+  //최초 1회 화면이 렌더링되는 시점(마운팅되는시점)에 실행되는 useEffect함수
+  //프로젝트 루트에 next.config.mjs파일내 reactStrictMode(엄격모드)값을 false로 변경해야 정확히 1회만 실행됨
+  //채팅서버와 연결되는 클라이언트 채팅 소켓 객체 생성 및 각종 채팅 이벤트 기능 구현영역
   useEffect(() => {
-    // 웹브라우저 저장소에 저장된 서버에서 발급해준 JWT 사용자인증정보 토큰 추출
+    //웹브라우저 저장소에 저장된 서버에서 발급해준 JWT사용자인증정보 토큰추출하기
     const token = localStorage.getItem("token");
-
     if (token == undefined) {
       router.push("/login");
     }
 
+    console.log("전역 데이터 정보 확인하기:", globalData);
     setMemberToken(token as string);
 
-    // 최초 화면이 렌더링되는 시점에 서버소켓 연결
+    //최초 화면이 렌더링되는 시점(최초1회)에 서버소켓 연결하기
     socket.connect();
 
-    // 서버소켓과 연결이 완료되면 실행되는 이벤트함수
-    // 서버소켓과 연결이 완료되면 자동으로 client 소켓에서 connect 이벤트가 실행되고 connect이벤트가 실행되면 처리할 이벤트처리할 기능 구현
-    // 소켓 시스템 이벤트
+    //서버소켓과 연결이 완료되면 실행되는 이벤트처리함수
+    //서버 소켓과 연결이 완료되면 자동으로 client 소켓에서connect이벤트가 실행되고
+    //connect이벤트가 실행되면 처리할 이벤트 처리할 기능 구현
+    //소켓 시스템 이벤트
     socket.on("connect", () => {
-      console.log("정상적으로 서버소켓과 연결되었습니다.");
-
-      // 사용자 채팅방에 입장하기 처리
+      console.log("정상적으로 서버소켓과 연결이 완료되었습니다.");
+      console.log("전역 데이터 정보 확인하기:", globalData);
     });
 
-    // disconnect 이벤트는 서버소켓이 끊어진 경우 발생하는 이벤트
-    // 서버와의 연결소켓이 끊어진 경우 처리할 기능을 핸들러함수에서 처리
-    // 소켓 시스템 이벤트
+    //disconnect 이벤트는 서버소켓이 끊어진경우 발생하는 이벤트
+    //서버와의 연결소켓이 끊어진경우 처리할 기능을 핸들러함수에서처리합니다.
+    //소켓 시스템 이벤트
     socket.on("disconnect", () => {
       console.log("서버소켓 연결이 종료되었습니다.");
     });
 
-    // 개발자정의 클라이언트 소켓 이벤트 수신기 정의하기
-    // socket.on("클라이언트 이벤트 수신기명", 서버에서 전달해준 데이터);
+    //개발자 정의 클라이언트 소켓 이벤트 수신기 정의하기
+    //socket.on('클라이언트 이벤트 수신기명',서버에서 전달해준 데이터를 받는함수정의);
     socket.on("receiveAll", function (msg: IMessage) {
       console.log("서버소켓에서 전달된 데이터 확인-receiveAll:", msg);
       setMessageList((prev) => [...prev, msg]);
     });
 
-    // 해당 채팅 컴포넌트가 화면에서 사라질 때(언마운팅시점)
-    // 소켓관련 이벤트를 모두 제거해야 메시지를 여러 번 수신 가능
-    // socket.off(클라이언트이벤트수신기명); : 이벤트제거
+    //사용자 지정 채널 입장완료 이벤트 수신기
+    socket.on("entryOk", function (msg: IMessage) {
+      console.log("서버소켓에서 전달된 데이터 확인-receiveAll:", msg);
+      setMessageList((prev) => [...prev, msg]);
+    });
+
+    //채팅채널별 메시지 수신기 정의 하기
+    socket.on("receiveChannel", function (msg: IMessage) {
+      console.log("서버소켓에서 전달된 데이터 확인-receiveChannel:", msg);
+      setMessageList((prev) => [...prev, msg]);
+    });
+
+    //해당 채팅 컴포넌트가 화면에서 사라질때(언마운팅시점)
+    //소켓관련 이벤트를 모두 제거해워야합니다. 그렇지 않으면 메시지를 여러번 수신할수 있음
     return () => {
+      //socket.off(클라이언트 이벤트 수신기명); //이벤트 제거
       socket.off("connect");
       socket.off("disconnect");
       socket.off("receiveAll");
+      socket.off("entryOk");
+      socket.off("receiveChannel");
     };
   }, []);
 
-  // 채팅 메시지 전송 이벤트 처리 함수
+  //채팅 메시지 전송 이벤트 처리함수
   const sendMessage = () => {
-    // 채팅서버소켓으로 메시지를 전송합니다.
-    // socket.emit("서버 이벤트수신기명", 전달할 데이터);
+    //채팅서버소켓으로 메시지를 전송합니다.
     const msgData = {
       member_id: globalData.member.member_id,
       name: globalData.member.name,
@@ -134,10 +129,11 @@ const Chat = () => {
       send_date: Date.now().toString(),
     };
 
-    // 채팅서버소켓으로 메시지 전송하기
-    socket.emit("broadcast", msgData);
+    //socket.emit('서버 이벤트 수신기명',전달할 데이터);
+    //채팅서버소켓으로 메시지 전송하기
+    socket.emit("channelMsg", channel.toString(), msgData);
 
-    // 메시지 입력박스 초기화
+    //메시지 입력박스 초기화
     setMessage("");
   };
 
@@ -187,6 +183,32 @@ const Chat = () => {
                       </div>
                     )
                   )}
+
+                  {/* 왼쪾 다른 사용자 메시지 출력 영역 */}
+                  {/* <div className="col-start-1 col-end-8 p-3 rounded-lg">
+                    <div className="flex flex-row items-center">
+                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                        A
+                      </div>
+                      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                        <div>Hey How are you today?</div>
+                      </div>
+                    </div>
+                  </div> */}
+
+                  {/* 오른쪽 본인 메시지 출력영역 */}
+                  {/* <div className="col-start-6 col-end-13 p-3 rounded-lg">
+                    <div className="flex items-center justify-start flex-row-reverse">
+                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                        A
+                      </div>
+                      <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                        <div>
+                          Lorem ipsum dolor sit, amet consectetur adipisicing. ?
+                        </div>
+                      </div>
+                    </div>
+                  </div> */}
                 </div>
               </div>
             </div>
